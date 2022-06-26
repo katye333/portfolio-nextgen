3195e42797660902f842e9daab4c256a24811d62
Katye Stevens <katye333@gmail.com>
2022-06-26 16:25:58 -0400

    feat: Particles completed in a separate component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Background } from './background';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Background/>
+            <div className='container'>
+                <p>HELLO </p>
+            </div>
+        </div>
+    )
+}
+
+export default App;
